5d7cc6d5ea32000baec9b1d7d88374d8e45c6e88
Jesse <realjesseshelley@gmail.com>
2024-04-25 17:38:25 -0600

    Update pyxor.py

--- pyxor.py
+++ pyxor.py
@@ -2,18 +2,18 @@ import sys
 
 
 class Xor:
-    def __init__(self):
+    def __init__(self) -> None:
         self.code = bytearray(
-            b"\x11\x10\n_F\x1bC\n\x06]^\\CKiUWUA\x15\x0cC^WIKY;\x12\x13AB\x11TFF\x13\x0cC\x13E\\\x13\x0e\x07\x13")
+            b"\x08\x0f\x13^@GA\x17\x11]^Z\x03L\x11TCF\x04\x11\x17;BA\x08\x0c\x17\x19\x10{\x04\x0e\x0f^\x1e\x13\r\x07\x17B\x12T\x0eB\x17^\x12T\x0e\r\x04]W\x1d\x02\r\x0e\x1f\x10\x1ak\x12\x11X\\GI\x17\x11]^Z\x03L\x11TCF\x04\x11\x17\x1fGA\r\r\x13T\\\x1bC\n\x17EB@[MLV]\\\x06\x0e\x06\x1fQ\\\x0c@J\x1f@V\x00\x06K\x18\x1cW\x04\x01\x0cUW\x1bHK")
 
-    def xor(self, key):
+    def xor(self, key) -> bytearray:
         result = bytearray()
 
         for i in range(len(self.code)):
             data_byte = self.code[i]
             key_byte = key[i % len(key)]  # Cycle through the key
-            xor_result = data_byte ^ key_byte
-            result.append(xor_result)
+            ciphertext = data_byte ^ key_byte
+            result.append(ciphertext)
 
         return result
 
@@ -33,8 +33,6 @@ def main() -> None:
 
     try:
         exec(xor.xor(key), globals_dict, locals_dict)
-        world_func = locals_dict['world']
-        print(world_func())
     except SyntaxError:
         print(f"Wrong passphrase.")
         sys.exit(1)
